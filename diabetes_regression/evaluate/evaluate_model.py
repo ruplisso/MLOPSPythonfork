f6a8a4f2e067e3df81a39b0abde889f5a2580a16
ruplisso <44896278+ruplisso@users.noreply.github.com>
2021-04-19 20:27:58 +0200

    Update evaluate_model.py

--- diabetes_regression/evaluate/evaluate_model.py
+++ diabetes_regression/evaluate/evaluate_model.py
@@ -132,7 +132,7 @@ try:
                 )
             )
 
-        if (new_model_mse < production_model_mse):
+        if (new_model_mse <= production_model_mse):
             print("New trained model performs better, "
                   "thus it should be registered")
         else:
